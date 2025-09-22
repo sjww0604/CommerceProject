0b8a1f9f8bacca8032c503a351f03d1d36fbe91f
sjww0306 <sjww03@gmail.com>
2025-09-22 12:20:46 +0900

    feat : 배열 노출 이후 프로그램 종료 기능 추가
    조건문 반복 노출 및 0을 입력받는 경우 프로그램이 종료 될 수 있도록 조건문을 추가함

--- src/com/commerce/example/Main.java
+++ src/com/commerce/example/Main.java
@@ -2,10 +2,12 @@ package com.commerce.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // 속성
+        Scanner sc = new Scanner(System.in);
         List<Product> productsList = new ArrayList<>();
 
         // 생성자
@@ -18,10 +20,19 @@ public class Main {
         // 기능
         System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
 
-        int index = 1; // 저장된 배열의 순서번호를 쓰기 위해 선언
         /* 향상된 for문으로 배열에 추가된 값을 처음부터 끝까지 반복해서 탐색하도록 설정 */
-        for (Product product : productsList) {
-            System.out.println(index++ + ". " + product.getPdName() + " | " + product.getPdPrice() + " | " + product.getPdDescription());
+        boolean mainStatus=true;
+        while (mainStatus) {
+            int index = 1; // 저장된 배열의 순서번호를 쓰기 위해 선언
+            for (Product product : productsList) {
+                System.out.println(index++ + ". " + product.getPdName() + " | " + product.getPdPrice() + " | " + product.getPdDescription());
+            }
+            System.out.println("0. 종료 | 프로그램 종료");
+            int choice = sc.nextInt(); // 메뉴 선택한 값을 저장하는 변수 선언
+            if (choice == 0) { // 받은 값이 0인 경우 반복문 실행을 종료시키는 연결과정으로 설정
+                mainStatus=false; // 종료인 경우 상태값을 저장하는 mainStatus를 false로 설정 while문을 탈출
+            }
         }
+        System.out.println("커머스 플랫폼을 종료합니다.");
     }
 }
