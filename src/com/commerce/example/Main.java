9f34ac1adf4e285bc08f32a13bbd83b92019410d
sjww0306 <sjww03@gmail.com>
2025-09-22 13:00:55 +0900

    feat : 배열 출력 및 가격단위, 글자폭 고정 설정을 통한 출력단계 표준화
    String.format을 활용한 다양한 기능 구현 연습, 표현방식에 따른 구현과정 확인

--- src/com/commerce/example/Main.java
+++ src/com/commerce/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         List<Product> productsList = new ArrayList<>();
 
         // 생성자
-        productsList.add(new Product("Gaxlay S25", 1200000, "최신 안드로이드 스마트폰", 0));
+        productsList.add(new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 0));
         productsList.add(new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 0));
         productsList.add(new Product("MackBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 0));
         productsList.add(new Product("Airpods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 0));
@@ -25,7 +25,12 @@ public class Main {
         while (mainStatus) {
             int index = 1; // 저장된 배열의 순서번호를 쓰기 위해 선언
             for (Product product : productsList) {
-                System.out.println(index++ + ". " + product.getPdName() + " | " + product.getPdPrice() + " | " + product.getPdDescription());
+                /* String.format을 활용한 자릿수 및 단위 표현 기능 추가
+                * %-15s : 고정폭 15만큼 설정, 왼쪽 정렬로 나타냄
+                * %,10d : 고정폭 10만큼 설정, 자릿수 , 표현 및 원 단위 붙이기 (Decimal 십진수의 글자를 딴 표현형태)
+                * %s : 문자형(String)으로 표현*/
+                String productResultList = String.format("%d. %-15s | %,10d원 | %s", index++, product.getPdName(), product.getPdPrice(), product.getPdDescription());
+                System.out.println(productResultList);
             }
             System.out.println("0. 종료 | 프로그램 종료");
             int choice = sc.nextInt(); // 메뉴 선택한 값을 저장하는 변수 선언
@@ -34,5 +39,7 @@ public class Main {
             }
         }
         System.out.println("커머스 플랫폼을 종료합니다.");
+
+        // ++ 자릿수 표현 기능 검색해보고 사용해보기
     }
 }
