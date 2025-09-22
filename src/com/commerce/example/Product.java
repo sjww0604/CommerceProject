9798b29d375882ef7f8f501a644ec74882ee5f49
sjww0306 <sjww03@gmail.com>
2025-09-22 12:43:43 +0900

    docs : 변수명 선언관련 내용 추가 정리(주석 수정)
    변수명 선언 시 유의해야할 것, 구분지어야 하는 기준에 대해서 추가 정리함

--- src/com/commerce/example/Product.java
+++ src/com/commerce/example/Product.java
@@ -4,7 +4,10 @@ package com.commerce.example;
 * 상품명, 가격, 설명, 재고수량
 * 예시) new Product("Galaxy S24", 1200000, "최신 스마트폰", 50 */
 public class Product {
-    /* 상품명 : pdName, 가격 : pdPrice, 설명 : pdDescription, 재고수량 : pdQty 설정 */
+    /* 상품명 : pdName, 가격 : pdPrice, 설명 : pdDescription, 재고수량 : pdQty 설정
+    * 변수명의 경우는 약속된 부분이 아니라면 축약어 사용은 지양할 것
+    * 상황에 따라 다르겠으나 한번만 정의되고 추가적인 개념이 쓰이지 않는다면 일반적인 네이밍을 사용
+    * 기능과 그 방향이 달라 변수명 선언하는데 주의가 필요한 경우는 구분을 확실하게 하기 */
     private String pdName;
     private int pdPrice;
     private String pdDescription;
