91a7dbbe67434b490da9d195af7536657aca2621
sjww0306 <sjww03@gmail.com>
2025-09-22 11:56:31 +0900

    feat : 상품 목록 출력 기본 구현 설정
    상품 리스트를 저장하고 저장한 배열의 값들을 향상된 for문을 통해 출력할 수 있도록 설정 진행 완료
    
    추가 구현 필요사항 : 금액 자릿수 구분, 반복문 외에 0. 종료 메뉴 추가

--- src/com/commerce/example/Main.java
+++ src/com/commerce/example/Main.java
@@ -1,7 +1,27 @@
 package com.commerce.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        // 속성
+        List<Product> productsList = new ArrayList<>();
 
+        // 생성자
+        productsList.add(new Product("Gaxlay S25", 1200000, "최신 안드로이드 스마트폰", 0));
+        productsList.add(new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 0));
+        productsList.add(new Product("MackBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 0));
+        productsList.add(new Product("Airpods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 0));
+
+
+        // 기능
+        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+
+        int index = 1; // 저장된 배열의 순서번호를 쓰기 위해 선언
+        /* 향상된 for문으로 배열에 추가된 값을 처음부터 끝까지 반복해서 탐색하도록 설정 */
+        for (Product product : productsList) {
+            System.out.println(index++ + ". " + product.getPdName() + " | " + product.getPdPrice() + " | " + product.getPdDescription());
+        }
     }
 }
